dfc0f78f931356718dae4627c8ee92e3e11e1fb8
ManyuDhyani <manyudhyani@gmail.com>
2024-07-29 00:48:08 -0400

    Cart State

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
 
 function App() {
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems)
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <div className="App">
